80b946e9f77c637e13e6c7e693730895b07932a6
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-07-29 21:04:21 +0300

    Use telegram-lambda-helpers to set webhook

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/yevhenshymotiuk/drug-lord-of-armavir-bot
 go 1.14
 
 require (
-	github.com/aws/aws-lambda-go v1.17.0
+	github.com/aws/aws-lambda-go v1.18.0
 	github.com/aws/aws-sdk-go v1.32.3
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.3+incompatible
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/yevhenshymotiuk/telegram-lambda-helpers v0.0.0-20200729171949-b469476e28a1
 )
--- setwebhook/main.go
+++ setwebhook/main.go
@@ -1,64 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
 	"os"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/yevhenshymotiuk/drug-lord-of-armavir-bot/apigateway"
+	"github.com/yevhenshymotiuk/telegram-lambda-helpers"
 )
 
-func handler(
-	request events.APIGatewayProxyRequest,
-) (apigateway.Response, error) {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
-	if err != nil {
-		log.Panic(err)
-	}
-
-	url := fmt.Sprintf(
-		"https://%s/%s/",
-		request.Headers["Host"],
-		request.RequestContext.Stage,
-	)
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
-	}
-
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(
-		map[string]interface{}{"message": "Webhook was successfully set!"},
-	)
-	if err != nil {
-		return apigateway.Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := apigateway.Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers:         map[string]string{"Content-Type": "application/json"},
-	}
-
-	return resp, nil
-}
-
 func main() {
-	lambda.Start(handler)
+	lambda.Start(helpers.SetWebhook(os.Getenv("TELEGRAM_TOKEN")))
 }
